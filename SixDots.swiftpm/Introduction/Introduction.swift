9cf8902457e22b9931726220b5cfe169c55f7deb
Dayeon Moon <0217dayun@naver.com>
2023-04-07 15:02:53 +0900

    [CHORE] initial commit, apply gitignore

---
+++ SixDots.swiftpm/Introduction/Introduction.swift
@@ -0,0 +1,20 @@
+//
+//  Introduction.swift
+//  
+//
+//  Created by 제나 on 2023/04/07.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
