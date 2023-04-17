2e42199a63189e4b9fb6f827dc75fce1b0de4282
Dayeon Moon <0217dayun@naver.com>
2023-04-18 00:32:14 +0900

    [CHORE] 이미지에셋, 앱컬러 및 모양 변경, 필요없는 파일 삭제

--- SixDots.swiftpm/Package.swift
+++ SixDots.swiftpm/Package.swift
@@ -20,8 +20,8 @@ let package = Package(
             teamIdentifier: "7D5PZJWQ9T",
             displayVersion: "1.0",
             bundleVersion: "1",
-            appIcon: .placeholder(icon: .tv),
-            accentColor: .presetColor(.green),
+            appIcon: .placeholder(icon: .twoPeople),
+            accentColor: .asset("AccentColor"),
             supportedDeviceFamilies: [
                 .pad,
                 .phone
